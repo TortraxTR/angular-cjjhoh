e768b7ec396b9c8954df4cf77a3e494930199dad
TortraxTR <iceburn322@gmail.com>
2020-08-19 13:55:15 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-cjjhoh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-cjjhoh)
---
+++ src/app/products.ts
@@ -0,0 +1,30 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: 'Bang for buck'
+  },
+  {
+    name: 'Phone 10',
+    price: 899,
+    description: 'The best there is',
+  }
+
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
